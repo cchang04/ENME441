ce3cd8c71493952ce67a52f1f0b171ba53d7f31d
cchang04 <cchang04@terpmail.umd.edu>
2025-10-07 15:58:23 -0400

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -6,25 +6,31 @@ import RPi.GPIO as GPIO
 import time
 import math
 
-
-
-p = 17
+p = [17, 27]
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(p, GPIO.OUT)
 
-pwm = GPIO.PWM(p, 500)
+for i in range(2):
+	GPIO.setup(p, GPIO.OUT)
 
-pwm.start(0)
+pwm1 = GPIO.PWM(p[0], 500)
+pwm2 = GPIO.PWM(p[1], 500)
+
+pwm1.start(0)
+pwm2.start(0)
 
 try:
 	while True:
 		f = 0.2
 		t = time.time()
-		B = (math.sin(2*math.pi*f*t))**2
-		dc = B*100
+		B1 = (math.sin(2*math.pi*f*t))**2
+		phi = math.pi/11
+		B2 = (math.sin((2*math.pi*f*t) - phi))**2
+		dc1 = B*100
+		dc2 = B2*100
 
-		pwm.ChangeDutyCycle(dc)
+		pwm1.ChangeDutyCycle(dc1)
+		pwm2.ChangeDutyCycle(dc2)
 
 except KeyboardInterrupt:
 	print('\nExiting')
