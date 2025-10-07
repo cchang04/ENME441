5549c17779a2194c916ce57ad3d0baf210031060
cchang04 <cchang04@terpmail.umd.edu>
2025-10-07 15:23:08 -0400

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -6,13 +6,13 @@ import RPi.GPIO as GPIO
 import time
 import math
 
-t = time.time()
-f = 0.2
-B = (math.sin(2*math.pi*f*t))**2
 p = 17
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(p, GPIO.OUT)
 
 while True:
+	t = time.time()
+	f = 0.2
+	B = (math.sin(2*math.pi*f*t))**2
 	GPIO.output(p, B)
