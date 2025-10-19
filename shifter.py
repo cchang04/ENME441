9b621fa8754e0725d9804ebd9957ee5b2a8a8d80
cchang04 <cchang04@terpmail.umd.edu>
2025-10-19 17:25:06 -0400

    Update shifter.py

--- shifter.py
+++ shifter.py
@@ -6,10 +6,10 @@ GPIO.setmode(GPIO.BCM)
 serialPin, latchPin, clockPin = 23, 24, 25
 
 class Shifter:
-  def __init__(self, serialPin, clockPin, latchPin):
+  def __init__(self, serialPin, latchPin, clockPin):
     self.serialPin = serialPin
-    self.clockPin = clockPin
     self.latchPin = latchPin
+    self.clockPin = clockPin
     
     GPIO.setup(serialPin, GPIO.OUT)
     GPIO.setup(latchPin, GPIO.OUT, initial=0)
@@ -22,13 +22,6 @@ class Shifter:
 
   def shiftByte(self, b):
     for i in range(8):
-      bit_value = 1 if (b & (1<<i)) else 0
-      """
-      the (1<<i) shifts the binary number "1" by i values to the left to create a byte with a singular 1 at the ith position
-      b is equal to the byte we are looking at (ex. in class was 01100110)
-      if the ith bit in b is 1 AND the left shifted bit is also 1, bit_value returns a 1
-      if the ith bit in b is a 0, then we have a 0 and a 1 which results in a 0
-      """
       GPIO.output(self.serialPin, b & (1 << i)) #sets the designated serial pin to either 0 or 1 (high/low, on/off for LED)
       self.__ping(self.clockPin)
     self.__ping(self.latchPin)
